27b005892489ac22a6f3df18039e0ff429385b9c
wail-10 <koundiwail02@gmail.com>
2024-03-03 12:00:41 +0000

    first commit

---
+++ src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -0,0 +1,45 @@
+package ma.enset.hospital;
+
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
+public class HospitalApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "Ahmed", new Date("10/10/2002"),false, 8));
+        patientRepository.save(new Patient(null, "Wail", new Date("27/10/2002"),false, 10));
+        patientRepository.save(new Patient(null, "Oussama", new Date("20/10/2002"),false, 10));
+        List<Patient> patients = patientRepository.findAll();
+        patients.forEach(p->{
+            System.out.println(p.toString());
+        });
+        System.out.println("*****************");
+        Patient patient = patientRepository.findById(Long.valueOf(1)).get();
+        System.out.println(patient.toString());
+        System.out.println("*****************");
+        List<Patient> patientList = patientRepository.findByNom("Wail");
+        patientList.forEach(p->{
+            System.out.println(p.toString());
+        });
+        System.out.println("*****************");
+        Patient patient1 = patientRepository.updateById(Long.valueOf(2));
+        patient1.setMalade(true);
+        patient1.setScore(5);
+        System.out.println(patient1.toString());
+    }
+}
---
+++ src/main/java/ma/enset/hospital/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package ma.enset.hospital.repository;
+
+import ma.enset.hospital.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByNom(String nom);
+    Patient updateById(Long id);
+}
