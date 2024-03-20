e79792a095b913b1419818a85ed49c7a0aa2463e
wail-10 <koundiwail02@gmail.com>
2024-03-20 23:56:04 +0000

    fix role and user repository

--- src/main/java/ma/enset/hospital/repository/RoleRepository.java
+++ src/main/java/ma/enset/hospital/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import ma.enset.hospital.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
+    Role findByRoleName(String roleName);
 }
--- src/main/java/ma/enset/hospital/repository/UserRepository.java
+++ src/main/java/ma/enset/hospital/repository/UserRepository.java
@@ -4,4 +4,5 @@ import ma.enset.hospital.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
 }
